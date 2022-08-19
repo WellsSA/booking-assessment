12ba1d8e8115d53d05e142e32c6e93893ecf2f71
WellsSA <wel.cavzod@gmail.com>
2022-08-19 01:41:44 -0300

    refactor(): removing redux integration mvp code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useAppDispatch } from 'hooks';
-import React, { useEffect } from 'react';
-import { testAction } from 'store/slices/booking';
-
 import Routes from 'routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -9,21 +5,11 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 
-const TestReduxComponent: React.FC = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(testAction({ test: true }));
-  }, []);
-  return null;
-};
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router>
-          <TestReduxComponent />
           <Routes />
         </Router>
       </PersistGate>
