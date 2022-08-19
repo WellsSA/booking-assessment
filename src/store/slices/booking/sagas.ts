a295dacea9d9c0474f082f4f46ace41ed00ad449
WellsSA <wel.cavzod@gmail.com>
2022-08-19 04:45:46 -0300

    feat(store/booking): update and delete sagas

--- src/store/slices/booking/sagas.ts
+++ src/store/slices/booking/sagas.ts
@@ -1,6 +1,10 @@
 import {
   createBookingRequest,
   createBookingSuccess,
+  updateBookingRequest,
+  updateBookingSuccess,
+  deleteBookingRequest,
+  deleteBookingSuccess,
 } from 'store/slices/booking';
 import { takeLatest, all, put } from 'redux-saga/effects';
 import { v4 as uuidv4 } from 'uuid';
@@ -11,6 +15,8 @@ export function* createBooking({ payload: { property, interval } }) {
     if (interval.start > interval.end) {
       throw new Error('Start date must be before end date');
     }
+
+    // Note: here we could have a backend call.
 
     yield put(
       createBookingSuccess({
@@ -28,4 +34,37 @@ export function* createBooking({ payload: { property, interval } }) {
   }
 }
 
-export default all([takeLatest(createBookingRequest, createBooking)]);
+export function* updateBooking({ payload: { booking } }) {
+  try {
+    // Note: here we could have a backend call.
+    yield put(
+      updateBookingSuccess({
+        booking,
+      })
+    );
+
+    notifySuccess('Booking updated successfully!');
+  } catch (error) {
+    notifyError(error.message);
+  }
+}
+export function* deleteBooking({ payload: { booking } }) {
+  try {
+    // Note: here we could have a backend call.
+    yield put(
+      deleteBookingSuccess({
+        booking,
+      })
+    );
+
+    notifySuccess('Booking deleted successfully!');
+  } catch (error) {
+    notifyError(error.message);
+  }
+}
+
+export default all([
+  takeLatest(createBookingRequest, createBooking),
+  takeLatest(updateBookingRequest, updateBooking),
+  takeLatest(deleteBookingRequest, deleteBooking),
+]);
