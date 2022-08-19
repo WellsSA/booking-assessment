a9aca2d3971b1cc9a6ac7bb9c18effc064d57c5d
WellsSA <wel.cavzod@gmail.com>
2022-08-19 05:53:26 -0300

    feat(store/booking): adding overlapping verification on creation

--- src/store/slices/booking/sagas.ts
+++ src/store/slices/booking/sagas.ts
@@ -6,9 +6,10 @@ import {
   deleteBookingRequest,
   deleteBookingSuccess,
 } from 'store/slices/booking';
-import { takeLatest, all, put } from 'redux-saga/effects';
+import { takeLatest, all, put, select } from 'redux-saga/effects';
 import { v4 as uuidv4 } from 'uuid';
 import { notifyError, notifySuccess } from 'services/notify';
+import { areIntervalsOverlapping } from 'date-fns';
 
 export function* createBooking({ payload: { property, interval } }) {
   try {
@@ -16,8 +17,28 @@ export function* createBooking({ payload: { property, interval } }) {
       throw new Error('Start date must be before end date');
     }
 
-    // Note: here we could have a backend call.
+    const { bookings } = yield select(state => state.booking);
 
+    bookings.forEach(booking => {
+      if (
+        areIntervalsOverlapping(
+          {
+            start: new Date(interval.start),
+            end: new Date(interval.end),
+          },
+          {
+            start: new Date(booking.interval.start),
+            end: new Date(booking.interval.end),
+          }
+        )
+      ) {
+        throw new Error(
+          'Booking failed. Please note you have an overlapping Booking.'
+        );
+      }
+    });
+
+    // Note: here we could have a backend call.
     yield put(
       createBookingSuccess({
         booking: {
