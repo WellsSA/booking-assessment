1dc7e0d65b7f47aa23a41fdc98ec214a25bd6452
WellsSA <wel.cavzod@gmail.com>
2022-08-19 06:55:56 -0300

    feat(atoms/Button): hover update for variants

--- src/components/atoms/_common/Button/styles.ts
+++ src/components/atoms/_common/Button/styles.ts
@@ -10,6 +10,7 @@ export const StyledButton = styled.button<Props>`
   border: none;
   padding: 10px;
   border-radius: 4px;
+  transition: color 0.2s ease-in-out, background 0.2s ease-in-out;
 
   ${({ variant }) => {
     switch (variant) {
@@ -17,17 +18,30 @@ export const StyledButton = styled.button<Props>`
         return css`
           color: var(--primary-text);
           background: var(--primary-color);
+
+          &:hover {
+            background: var(--primary-color-active);
+          }
         `;
       case 'danger':
         return css`
           color: var(--danger-text);
           background: var(--danger-color);
+
+          &:hover {
+            background: var(--danger-color-active);
+          }
         `;
       case 'outlined':
         return css`
           color: var(--danger-color);
           background: transparent;
           border: 1px solid var(--danger-color);
+
+          &:hover {
+            color: var(--danger-text);
+            background: var(--danger-color-weak);
+          }
         `;
       default:
         return null;
